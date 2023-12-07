09221bda6f5abf38c955655f10713d1e411c7eb4
Rithick S <89693307+te-yd@users.noreply.github.com>
2023-12-07 09:06:27 +0530

    Fix matrix

--- main.c
+++ main.c
@@ -4,12 +4,21 @@
 
 int main() {
     srand(time(0));
-    Mat m1 = Mat_alloc(2, 2);
-    Mat m2 = Mat_alloc(12, 12);
+    Mat w1 = Mat_alloc(2, 2);
+    Mat b1 = Mat_alloc(1, 2);
 
-    Mat_fill(&m1, 1);
-    Mat_fill(&m2, 3);
+    Mat w2 = Mat_alloc(2, 1);
+    Mat b2 = Mat_alloc(1, 1);
 
-    Mat_sum(&m1, m1, m2);
-    Mat_print(m1);
+    Mat_randn(&w1);
+    Mat_randn(&w2);
+    Mat_randn(&b1);
+    Mat_randn(&b2);
+
+    Mat_PRINT(w1);
+    Mat_PRINT(w2);
+    Mat_PRINT(b1);
+    Mat_PRINT(b2);
+
+    return 0;
 }
--- nn.h
+++ nn.h
@@ -19,6 +19,8 @@
 #define MAT(x) Mat_ ## x
 #endif
 
+#define Mat_PRINT(m) Mat_print(m, #m)
+
 #ifndef MATRef
 #define MATRef(x) Mat *## x
 #endif
@@ -33,12 +35,22 @@ typedef struct {
 } Mat;
 
 Mat MAT(alloc)(unsigned long rows, unsigned long cols);
-void MAT(dot)(Mat out, Mat in1, Mat in2);
+void MAT(dot)(Mat *out, Mat in1, Mat in2);
+Mat MAT(id)(Mat out);
 void MAT(sum)(const Mat * const out, Mat in1, Mat in2);
-void MAT(print)(Mat m);
+void MAT(print)(Mat m, char *name);
 void MAT(fill)(const Mat * const m, float value);
 void MAT(randn)(const Mat * const m);
 size_t MAT(size)(Mat m);
+
+inline Mat MAT(id)(Mat out) {
+    for (size_t i = 0; i < out.rows; i++) {
+        for (size_t j = 0; j < out.cols; j++) {
+            MAT_AT(out, i, j) = !!(i == j);
+        }
+    }
+    return out;
+}
 
 inline Mat MAT(alloc)(unsigned long rows, unsigned long cols) {
     Mat m;
@@ -48,14 +60,25 @@ inline Mat MAT(alloc)(unsigned long rows, unsigned long cols) {
     return m;
 }
 
-inline void MAT(dot)(Mat out, Mat in1, Mat in2) {
-
+inline void MAT(dot)(Mat *out, Mat in1, Mat in2) {
+    NN_ASSERT(in1.cols == in2.rows);
+    size_t n = in1.cols;
+    NN_ASSERT(out->rows == in1.rows && out->cols == in2.cols);
+    for (size_t i = 0; i < out->rows; ++i) {
+        for (size_t j = 0; j < out->cols; ++j) {
+            float value = 0;
+            for (size_t k = 0; k < n; k++) {
+                value += MAT_AT(in1, i, k) * MAT_AT(in2, k, j);
+            }
+            MATRef_AT(out, i, j) = value;
+        }
+    }
 }
 
 inline void MAT(sum)(const Mat * const out, Mat in1, Mat in2) {
     MAT_NON_NULL(out);
-    NN_ASSERT(in1.rows == in1.cols && in2.cols == in2.cols);
-    NN_ASSERT(in1.rows == in1.cols && out->cols == out->cols);
+    NN_ASSERT(in1.rows == in2.rows && in1.cols == in2.cols);
+    NN_ASSERT(in1.rows == out->rows && in1.cols == out->cols);
 
     for (size_t i = 0; i < out->rows; i++) {
         for (size_t j = 0; j < out->cols; j++) {
@@ -64,13 +87,15 @@ inline void MAT(sum)(const Mat * const out, Mat in1, Mat in2) {
     }
 }
 
-inline void MAT(print)(Mat m) {
+inline void MAT(print)(Mat m, char *name) {
+    printf("%s = [\n", name);
     for (int i = 0; i < m.rows; i++) {
         for (int j = 0; j < m.cols; j++) {
-            printf("%f ", MAT_AT(m, i, j));
+            printf("  %f", MAT_AT(m, i, j));
         }
         printf("\n");
     }
+    printf("]\n");
 }
 
 inline void MAT(randn)(const Mat * const m) {
